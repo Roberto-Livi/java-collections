58317e3dde895e1aadec86ca58c48bd9d3e0044f
Roberto-Livi <robertomlivi@outlook.com>
2020-11-10 17:35:40 -0500

    testing the ability to reserve a seat in the Main method

--- code/src/com/livi/Main.java
+++ code/src/com/livi/Main.java
@@ -3,6 +3,13 @@ package com.livi;
 public class Main {
 
     public static void main(String[] args) {
+        Theater theater = new Theater("AMC", 8, 12);
+//        theater.getSeats();
+        if(theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
 
     }
 
